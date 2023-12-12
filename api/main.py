d726a8c6c31dfdd4d47179590043745c70fd9a5c
hunter <50197260+Hunter-Fang@users.noreply.github.com>
2023-12-12 15:37:10 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)  # 添加这一行来启用CORS
@@ -9,10 +10,10 @@ CORS(app)  # 添加这一行来启用CORS
 def get_data():
     try:
         headers = {
-            'Authorization': 'Bearer secret_FuF38QkizWL5lgsK8oQd6IhjX4jVKa4mywyfag6HtJr',
+            'Authorization': f'Bearer {os.environ.get("NOTION_TOKEN")}',
             'Notion-Version': '2021-05-13'
         }
-        response = requests.post('https://api.notion.com/v1/databases/e649801ccfa24c59b903148531d60783/query', headers=headers)
+        response = requests.post(f'https://api.notion.com/v1/databases/{os.environ.get("NOTION_DATABASE_ID")}/query', headers=headers)
         return response.json()
     except Exception as e:
         print(e)
